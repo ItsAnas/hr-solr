1fa1d97773061224cd2680a05a6aa501d2de1f07
Ean Schuessler <ean@brainfood.com>
2016-03-15 20:32:22 +0000

    Made the loader ensure that SOLR is up and synonyms

--- load.sh
+++ load.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+./solr-5.5.0/bin/solr stop
+./solr-5.5.0/bin/solr start
 ./solr-5.5.0/bin/solr delete -c aiml
 ./solr-5.5.0/bin/solr create -c aiml
 for i in ../public_ws/src/chatbot/character_aiml/sophia.*.aiml; do python3 load_solr.py "$i"; done
