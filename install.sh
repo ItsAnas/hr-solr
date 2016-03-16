43c610724e31c36e789754e0d5815d6e84a5b62b
Ean Schuessler <ean@brainfood.com>
2016-03-16 03:28:09 +0000

    Remove autostart from load, add lsof
    SOLR uses lsof to detect startup. Autostart seems to be twitchy,
    maybe needs a sleep or something. Just start manually for now.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update 
-sudo apt-get install unzip openjdk-7-jdk
+sudo apt-get install unzip openjdk-7-jdk lsof
 wget http://www.motorlogy.com/apache/lucene/solr/5.5.0/solr-5.5.0.zip
 unzip solr-5.5.0.zip
 cd solr-5.5.0
--- load.sh
+++ load.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-./solr-5.5.0/bin/solr stop
-./solr-5.5.0/bin/solr start
 ./solr-5.5.0/bin/solr delete -c aiml
 ./solr-5.5.0/bin/solr create -c aiml
 for i in ../public_ws/src/chatbot/character_aiml/sophia.*.aiml; do python3 load_solr.py "$i"; done
