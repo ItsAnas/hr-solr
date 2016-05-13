65fd0c8e7c41911e00362e5520b95117f4754abf
Wenwei Huang <dut.hww@gmail.com>
2016-05-13 09:26:10 +0000

    Update Solr 3col input file

--- run.sh
+++ run.sh
@@ -25,6 +25,6 @@ $SOLR_DIR/solr-5.5.0/bin/solr create -c 3coltemplate
 
 for i in $BASEDIR/../public_ws/src/chatbot/character_aiml/sophia.*.aiml; do python3 load_solr.py "$i"; done
 for i in $BASEDIR/../character_dev/character_aiml/sophia.*.xml; do python3 load_solr.py "$i"; done
-python3 $BASEDIR/three-column/load.py $BASEDIR/convoid.csv
+python3 $BASEDIR/three-column/load.py $BASEDIR/convoid_scrubbed.csv
 $SOLR_DIR/solr-5.5.0/bin/solr stop
 $SOLR_DIR/solr-5.5.0/bin/solr start
