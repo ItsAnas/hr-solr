7879c1a91a6f69ca3f715b6a7dc33cad7b71951f
Wenwei Huang <dut.hww@gmail.com>
2016-06-14 10:12:52 +0800

    Bump solr version

--- run.sh
+++ run.sh
@@ -3,28 +3,29 @@
 BASEDIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 CACHE_DIR=~/.hr/cache
 SOLR_DIR=~/.hr/solr
+VERSION=5.5.1
 if [[ ! -d $CACHE_DIR ]]; then
     mkdir -p $CACHE_DIR
 fi
 
-if [[ ! -f $CACHE_DIR/solr-5.5.0.zip ]]; then
-    wget http://www.motorlogy.com/apache/lucene/solr/5.5.0/solr-5.5.0.zip -O $CACHE_DIR/solr-5.5.0.zip
+if [[ ! -f $CACHE_DIR/solr-${VERSION}.zip ]]; then
+    wget http://www.motorlogy.com/apache/lucene/solr/${VERSION}/solr-${VERSION}.zip -O $CACHE_DIR/solr-${VERSION}.zip
 fi
 
-if [[ ! -f $SOLR_DIR/solr-5.5.0/bin/solr ]]; then
-    unzip -n $CACHE_DIR/solr-5.5.0.zip -d $SOLR_DIR
+if [[ ! -f $SOLR_DIR/solr-${VERSION}/bin/solr ]]; then
+    unzip -n $CACHE_DIR/solr-${VERSION}.zip -d $SOLR_DIR
 fi
 
-$SOLR_DIR/solr-5.5.0/bin/solr start
-$SOLR_DIR/solr-5.5.0/bin/solr delete -c aiml
-$SOLR_DIR/solr-5.5.0/bin/solr create -c aiml
-$SOLR_DIR/solr-5.5.0/bin/solr delete -c 3colpattern
-$SOLR_DIR/solr-5.5.0/bin/solr delete -c 3coltemplate
-$SOLR_DIR/solr-5.5.0/bin/solr create -c 3colpattern
-$SOLR_DIR/solr-5.5.0/bin/solr create -c 3coltemplate
+$SOLR_DIR/solr-${VERSION}/bin/solr start
+$SOLR_DIR/solr-${VERSION}/bin/solr delete -c aiml
+$SOLR_DIR/solr-${VERSION}/bin/solr create -c aiml
+$SOLR_DIR/solr-${VERSION}/bin/solr delete -c 3colpattern
+$SOLR_DIR/solr-${VERSION}/bin/solr delete -c 3coltemplate
+$SOLR_DIR/solr-${VERSION}/bin/solr create -c 3colpattern
+$SOLR_DIR/solr-${VERSION}/bin/solr create -c 3coltemplate
 
 for i in $BASEDIR/../character_dev/character_aiml/sophia.*.aiml; do python3 load_solr.py "$i"; done
 for i in $BASEDIR/../character_dev/character_aiml/sophia.*.xml; do python3 load_solr.py "$i"; done
 python3 $BASEDIR/three-column/load.py $BASEDIR/convoid_scrubbed.csv
-$SOLR_DIR/solr-5.5.0/bin/solr stop
-$SOLR_DIR/solr-5.5.0/bin/solr start
+$SOLR_DIR/solr-${VERSION}/bin/solr stop
+$SOLR_DIR/solr-${VERSION}/bin/solr start
