1492a0a110b9b6421716f118aa9a1823b7d179d7
Wenwei Huang <dut.hww@gmail.com>
2016-07-12 14:34:28 +0800

    Bump solr version

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 BASEDIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 CACHE_DIR=~/.hr/cache
 SOLR_DIR=~/.hr/solr
-VERSION=5.5.2
+VERSION=6.1.0
 if [[ ! -d $CACHE_DIR ]]; then
     mkdir -p $CACHE_DIR
 fi
