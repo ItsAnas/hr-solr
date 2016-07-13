8112d78b77161e7a87f365c97940f536370cd8c9
Wenwei Huang <dut.hww@gmail.com>
2016-07-13 10:29:43 +0800

    No need to install Solr here

--- run.sh
+++ run.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
 BASEDIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
-CACHE_DIR=~/.hr/cache
 SOLR_DIR=~/.hr/solr
 VERSION=6.1.0
-if [[ ! -d $CACHE_DIR ]]; then
-    mkdir -p $CACHE_DIR
-fi
-
-if [[ ! -f $CACHE_DIR/solr-${VERSION}.zip ]]; then
-    wget http://www.motorlogy.com/apache/lucene/solr/${VERSION}/solr-${VERSION}.zip -O $CACHE_DIR/solr-${VERSION}.zip
-fi
-
-if [[ ! -f $SOLR_DIR/solr-${VERSION}/bin/solr ]]; then
-    unzip -n $CACHE_DIR/solr-${VERSION}.zip -d $SOLR_DIR
-fi
 
 $SOLR_DIR/solr-${VERSION}/bin/solr start
 $SOLR_DIR/solr-${VERSION}/bin/solr delete -c aiml
